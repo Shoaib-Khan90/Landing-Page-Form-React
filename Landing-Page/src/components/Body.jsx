407a1d811b24f5da8d551995abf8c90c60a6ee4b
Shoaib Khan <shoaib7878687@gmail.com>
2026-04-23 14:28:49 +0500

    updated changes

--- Landing-Page/src/components/Body.jsx
+++ Landing-Page/src/components/Body.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Body.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Body = () => {
   return (
@@ -35,15 +37,98 @@ const Body = () => {
             <p>This is the free demo for everyone</p>
             </div>
             <button style={{width:"200px", marginLeft:"20px", padding:"10px", borderRadius:"10px", backgroundColor:'green', color: "white", border:"none", fontSize:"20px"}}>Start Free Trial</button>
-            <div className='line'>
-            
+            <div className='line'></div>
+
+            <div className='list'>
+            <ul>
+              <li style={{color:"gray", fontFamily:"sans-serif", marginLeft:"-25px"}}><FontAwesomeIcon style={{color:"green", marginLeft:"10px"}} icon={faCheck} /> 4 Team member</li>
+              <li style={{color:"gray", fontFamily:"sans-serif", marginLeft:"-25px", marginTop:"10px"}}><FontAwesomeIcon style={{color:"green", marginLeft:"10px"}} icon={faCheck} /> 4 GB Storage</li>
+              <li style={{color:"gray", fontFamily:"sans-serif", marginLeft:"-25px", marginTop:"10px"}}><FontAwesomeIcon style={{color:"green", marginLeft:"10px"}} icon={faCheck} /> Upto 6 pages</li>
+            </ul>
             </div>
         </div>
         <div className='card3'>
-
+        <div className='flex3'>
+        <img style={{marginLeft:"38%", marginTop:"20px", borderRadius:"50%"}} src="./Images/image.avif" width="70px" height="60px" alt="" />
+        </div>
+        <div className='head1'>
+        <h2>Leo Miranda</h2>
+        <h3>Frontend Developer</h3>
+        </div>
+        <div className='head2'>
+        <p>I really enjoy transforming ideas into functional software that exceeds expectations</p>
+        </div>
+        <div className='link'>
+        <a style={{color:"black", textDecoration:"none"}} href="https://github.com/Shoaib-Khan90"><FontAwesomeIcon  icon={faGithub} /> </a> 
+        <a style={{color:"black", textDecoration:"none"}} href="https://www.linkedin.com/feed/"><FontAwesomeIcon style={{marginLeft:"20px"}} icon={faLinkedin} /></a>
+        </div>
         </div>
         <div className='card4'>
+        <div className='flex4'>
+        <img style={{borderRadius:"40%"}} src="./Images/light.avif" width="70px" height="60px" alt="" />
+        </div>
+        <div className='head3'>
+        <h2>Light & dark mode</h2>
+        <p>That is the goof dark and light mode</p>
+        </div>
+        </div>
+        </div>
+        </div>
 
+        <div className='investors'>
+        <h1>Investors and founders</h1>
+        </div>
+        <div className='circle'>
+          <div className='circle1'>
+        <img src="./Images/circle.avif" width="50px" alt="" />
+        <p>Sponsor 1</p>
+        </div>
+        <div className='circle1'>
+        <img src="./Images/circle.avif" width="50px" alt="" />
+        <p>Sponsor 2</p>
+        </div>
+        <div className='circle1'>
+        <img src="./Images/circle.avif" width="50px" alt="" />
+        <p>Sponsor 3</p>
+        </div>
+        <div className='circle1'>
+        <img src="./Images/circle.avif" width="50px" alt="" />
+        <p>Sponsor 4</p>
+        </div>
+        <div className='circle1'>
+        <img src="./Images/circle.avif" width="50px" alt="" />
+        <p>Sponsor 5</p>
+        </div>
+        <div className='circle1'>
+        <img src="./Images/circle.avif" width="50px" alt="" />
+        <p>Sponsor 6</p>
+        </div>
+        </div>
+
+        <div className='aboutcompany'>
+        <div className='image1'>
+        <img src="./Images/pilot.png" width="300px" alt="" />
+        </div>
+        <div className='text'>
+        <h2>About <span>Comapany</span></h2>
+        <p>That company was so good for anywhere side and there product was so good and effective for everyone</p>
+        <div className='User'>
+        <div className='user'> 
+        <h2> 2.7K+ </h2>
+        <p>Users</p>
+        </div>
+        <div className='user'> 
+        <h2> 1.8K+ </h2>
+        <p>Subscribers</p>
+        </div>
+        <div className='user'> 
+        <h2> 112 </h2>
+        <p>Downloads</p>
+        </div>
+        <div className='user'> 
+        <h2> 4 </h2>
+        <p>Products</p>
+        </div>
         </div>
         </div>
         </div>
